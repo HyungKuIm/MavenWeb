3428b7d380d305ae4a27b316f8f074c4bf42ca4d
A <A@DESKTOP-NKSGQVR>
2024-10-16 17:25:29 +0900

    blog edit

--- MavenWeb/src/main/java/com/oraclejava/controller/BlogController.java
+++ MavenWeb/src/main/java/com/oraclejava/controller/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -48,6 +49,18 @@ public class BlogController {
 		Blog blog = blogService.read(blogSeq);
 		model.addAttribute("blog", blog);
 		return "blog/edit";
+	}
+	
+	@PostMapping("/edit/{blogSeq}")
+	public String postEdit(Blog blog,
+			Model model) {
+		boolean isSuccess = blogService.edit(blog);
+		
+		if (isSuccess) {
+			return "redirect:/read/" + blog.getBlogSeq();
+		}
+		
+		return "redirect:/list";
 	}
 	
 	
