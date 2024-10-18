6f3b67f9883196c7f5a171d24ca1adf360a25a37
A <A@DESKTOP-NKSGQVR>
2024-10-18 16:31:19 +0900

    jpql 적용

--- commentstore/src/main/java/com/oraclejava/commentstore/service/CommentRepository.java
+++ commentstore/src/main/java/com/oraclejava/commentstore/service/CommentRepository.java
@@ -2,6 +2,7 @@ package com.oraclejava.commentstore.service;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.oraclejava.model.Comment;
@@ -9,5 +10,6 @@ import com.oraclejava.model.Comment;
 public interface CommentRepository
 	extends CrudRepository<Comment, String>{
 	
+	@Query("select c from Comment c order by creationDate desc")
 	List<Comment> findAll();
 }
